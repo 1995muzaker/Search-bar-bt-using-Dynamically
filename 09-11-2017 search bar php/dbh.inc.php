7c87ae61449fdad635f4fde4eadcebcd560a96ab
Shaikh Muzaker <shaikhmuzaker@gmail.com>
2018-02-17 13:05:14 +0530

    Add files via upload

---
+++ 09-11-2017 search bar php/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbphpsearch";
+
+$conn = mysqli_connect($server, $username, $password, $dbname);  
---
+++ 09-11-2017 search bar php/header.php
@@ -0,0 +1,12 @@
+<?php
+	include 'dbh.inc.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
